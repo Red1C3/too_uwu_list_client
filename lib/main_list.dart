f6663f1e610546f6064e2e6e17574933d0575043
Mohammad Issawi <2000issawi@gmail.com>
2023-09-14 20:28:59 +0300

    Build listview from notes

--- lib/main_list.dart
+++ lib/main_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:too_uwu_list_client/note_model.dart';
+import 'package:too_uwu_list_client/note_widget.dart';
 import 'client.dart' as client;
 
 class MainList extends StatefulWidget {
@@ -24,8 +25,22 @@ class _MainListState extends State {
       appBar: AppBar(title: const Text("too-UwU-list")),
       body: RefreshIndicator(
         onRefresh: () => fetchNotes(),
-        child: ListView(),
+        child: ListView.builder(
+            itemCount: _notes.length,
+            itemBuilder: ((context, index) {
+              return NoteWidget(
+                  note: _notes[index],
+                  deleteFunc: (int id) async {
+                    //TODO
+                  });
+            })),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //await fetchNotes(); TODO do sync-wise
   }
 }
