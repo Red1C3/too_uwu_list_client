d8c5ab544f62d945d6e7b1de342478c8e6a9513b
Mohammad Issawi <2000issawi@gmail.com>
2023-09-15 05:15:56 +0300

    Add autofocus and on submit callback for add note dialog

--- lib/add_note_dialog.dart
+++ lib/add_note_dialog.dart
@@ -10,6 +10,17 @@ class AddNoteDialog extends StatefulWidget {
 class _AddNoteDialogState extends State {
   final _controller = TextEditingController();
 
+  void _addNote(BuildContext context) {
+    var text = _controller.text;
+    client.createNote(text).then((value) {
+      if (value) {
+        Navigator.pop(context);
+      } else {
+        //TODO show snackbar
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -17,22 +28,14 @@ class _AddNoteDialogState extends State {
       content: Column(
         children: [
           TextField(
+            autofocus: true,
             controller: _controller,
+            onSubmitted: (value) => _addNote(context),
           ),
           Row(
             children: [
               ElevatedButton(
-                  onPressed: () {
-                    var text = _controller.text;
-                    client.createNote(text).then((value) {
-                      if (value) {
-                        Navigator.pop(context);
-                      } else {
-                        //TODO show snackbar
-                      }
-                    });
-                  },
-                  child: const Text("Add")),
+                  onPressed: () => _addNote(context), child: const Text("Add")),
               ElevatedButton(
                   onPressed: () {
                     Navigator.pop(context);
